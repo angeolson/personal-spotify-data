836bead9f44560cb3265d20c83bd7c0724283c1a
angeolson <78054238+angeolson@users.noreply.github.com>
2022-07-06 16:21:11 -0400

    Update TS_EDA.py

--- TS_EDA.py
+++ TS_EDA.py
@@ -9,7 +9,9 @@ import matplotlib.pyplot as plt
 import scipy.stats as stats 
 import statistics
 # %pip install plotly 
+# %pip install statsmodels
 import plotly.express as px
+import statsmodels 
 
 # graphing
 sns.set_theme(style="darkgrid", palette="Paired")
@@ -20,6 +22,7 @@ df = pd.read_csv("time_series_data_clean.csv")
 
 
 #%%
+# define pivot table function
 
 # get vals that need to be averaged
 mean_vals = "danceability", "energy", "loudness", "speechiness", "instrumentalness", "liveness", "valence", "tempo", "duration_ms", "track_pop", "artist_pop", "release_year"
@@ -27,55 +30,70 @@ mean_vals = "danceability", "energy", "loudness", "speechiness", "instrumentalne
 # get vals for mode
 mode_vals = "key", "mode", "time_signature", "artist_genre", "artist"
 
-index = ("playlist_date", "playlist_month")
+def getPivot(mean_vals, mode_vals, index):
+    
+    # define pivot table of means
+    table_means = pd.pivot_table(df, index = index, values = mean_vals, aggfunc=np.mean)
+    
+    # define pivot table of modes
+    table_modes = pd.pivot_table(df, index = index, values = mode_vals, aggfunc=statistics.mode)
 
-table = pd.pivot_table(df, index = index,values = mean_vals, aggfunc=np.mean)
+    pivot_df = table_means.join(table_modes, on=index)
+    pivot_df.reset_index(inplace=True)
+    pivot_df = pivot_df.rename(columns = {'index': index})
 
-table2 = pd.pivot_table(df, index = ["playlist_date"], values = mode_vals, aggfunc=statistics.mode)
-
-pivot_df = table.join(table2, on= "playlist_date")
-pivot_df.reset_index(inplace=True)
-pivot_df = pivot_df.rename(columns = {'index':'playlist_date', 'playlist_month'})
+    return pivot_df
 
 #%%
-# plot artist pop
-fig = px.line(pivot_df, x="playlist_date", y="artist_pop", markers=True)
+# create pivot table by date
+# get index val
+index = "playlist_date"
+
+datePivot = getPivot(mean_vals=mean_vals, mode_vals=mode_vals, index=index)
+
+datePivot["playlist_date"] = pd.to_datetime(datePivot.playlist_date)
+
+# create pivot table by month
+# get index val
+index2 = "playlist_month"
+
+monthPivot = getPivot(mean_vals=mean_vals, mode_vals=mode_vals, index=index2)
+
+#%%
+# plots based on date
+
+for y in mean_vals:
+    fig = px.scatter(datePivot, x="playlist_date", y=y, title=f"{y} Over Time", height=500, width=500, trendline="lowess", trendline_options=dict(frac=0.5))
+    fig.show()
+
+
+# %%
+# plots based on month 
+for y in mean_vals:
+    fig = px.scatter(monthPivot, x="playlist_month", y=y,title=f"{y} By Month", height=500, width=500, trendline="lowess", trendline_options=dict(frac=0.5))
+    fig.show()
+
+# %%
+# plot popularity over time together 
+y = ['artist_pop', 'track_pop']
+
+fig = px.scatter(datePivot, x="playlist_date", y=y, title=f"{y} Over Time", height=500, width=500, trendline="lowess", trendline_options=dict(frac=0.5))
 fig.show()
 
 # %%
-# plot track pop
-fig = px.line(pivot_df, x="playlist_date", y="track_pop", markers=True)
+# plot valence and energy over time together 
+y = ['valence', 'danceability']
+
+fig = px.scatter(datePivot, x="playlist_date", y=y, title=f"{y} Over Time", height=500, width=500, trendline="lowess", trendline_options=dict(frac=0.5))
 fig.show()
 
 # %%
-# plot danceability
-fig = px.line(pivot_df, x="playlist_date", y="danceability", markers=True)
-fig.show()
+corr = df.corr()
+ax = sns.heatmap(
+    corr, 
+    vmin=-1, vmax=1, center=0,
+    cmap=sns.diverging_palette(20, 220, n=200),
+    square=True
+)
 
 # %%
-# plot valence
-fig = px.line(pivot_df, x="playlist_date", y="valence", markers=True)
-fig.show()
-
-# %%
-# plot duration
-fig = px.line(pivot_df, x="playlist_date", y="duration_ms", markers=True)
-fig.show()
-
-# %%
-# plot energy
-fig = px.line(pivot_df, x="playlist_date", y="energy", markers=True)
-fig.show()
-
-# %%
-# plot release year
-fig = px.line(pivot_df, x="playlist_date", y="release_year", markers=True)
-fig.show()
-
-# %%
-# plot tempo
-fig = px.line(pivot_df, x="playlist_date", y="tempo", markers=True)
-fig.show()
-# %%
-fig = px.line(pivot_df, x="playlist_date", y="tempo", markers=True)
-fig.show()
