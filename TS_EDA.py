78f873c811372688e169860c540b921255e8376a
angeolson <78054238+angeolson@users.noreply.github.com>
2022-07-15 10:52:03 -0400

    Update TS_EDA.py

--- TS_EDA.py
+++ TS_EDA.py
@@ -60,6 +60,28 @@ index2 = "playlist_month"
 monthPivot = getPivot(mean_vals=mean_vals, mode_vals=mode_vals, index=index2)
 
 #%%
+# Calculate artist variance in playlists, total number of tracks to normalize by date
+dateVariance = df.groupby('playlist_date')['artist'].nunique()
+dateTrackCount = df.groupby('playlist_date')['track_name'].nunique()
+
+# normalization: if 1, artist to track ratio is 1:1 (i.e. each artist has one track). The lower the number, the more times you're listening to songs by the same artist. 
+
+dateVarNorm = dateVariance/dateTrackCount
+
+#%%
+# Calculate artist variance in playlists, total number of tracks to normalize by month
+monthVariance = df.groupby('playlist_month')['artist'].nunique()
+monthTrackCount = df.groupby('playlist_month')['track_name'].nunique()
+
+# normalization: if 1, artist to track ratio is 1:1 (i.e. each artist has one track). The lower the number, the more times you're listening to songs by the same artist. 
+
+monthVarNorm = monthVariance/monthTrackCount
+
+#%%
+# merge artist variance to pivot tables above 
+
+
+#%%
 # plots based on date
 
 for y in mean_vals:
@@ -88,6 +110,7 @@ fig = px.scatter(datePivot, x="playlist_date", y=y, title="Valence and Danceabil
 fig.show()
 
 # %%
+# plot variable correlation 
 corr = df.corr()
 ax = sns.heatmap(
     corr, 
